4198c43ffe6e0b6ab1cd4033503f8ae1dbfbea9e
MarkOsBab <m.g4089188@gmail.com>
2022-04-11 20:22:15 -0300

    navbar v1 on mobile

--- public/js/main.js
+++ public/js/main.js
@@ -28,3 +28,21 @@ if (currentTheme) {
 
 
 toggleSwitch.addEventListener('change', switchTheme, false);
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".navbar__box");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+const navLink = document.querySelectorAll(".navbar__box");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
