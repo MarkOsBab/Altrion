341730fbf817272bfb14ddfc504301ad5eb050fd
MarkOsBab <m.g4089188@gmail.com>
2022-04-14 08:32:42 -0300

    Contact progress

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,14 @@ function switchTheme(e) {
     if (e.target.checked) {
         document.body.setAttribute('class', 'theme--dark');
         document.getElementById('switch-icon').setAttribute('class', 'fa-solid fa-moon');
+        document.getElementById('switch-image').setAttribute('src', './public/images/footer/Logo_Light.png');
         localStorage.setItem('class', 'theme--dark'); //add this
         localStorage.setItem('icon', 'fa-solid fa-moon')
     }
     else {
         document.body.setAttribute('class', 'theme');
         document.getElementById('switch-icon').setAttribute('class', 'fa-solid fa-sun');
+        document.getElementById('switch-image').setAttribute('src', './public/images/footer/Logo_Dark.png');
         localStorage.setItem('class', 'theme'); //add this
         localStorage.setItem('icon', 'fa-solid fa-sun')
     }    
@@ -19,10 +21,12 @@ const currentTheme = localStorage.getItem('class') ? localStorage.getItem('class
 if (currentTheme) {
     document.body.setAttribute('class', currentTheme);
     document.getElementById('switch-icon').setAttribute('class', 'fa-solid fa-sun');
+    document.getElementById('switch-image').setAttribute('src', './public/images/footer/Logo_Dark.png');
 
     if (currentTheme === 'theme--dark') {
         toggleSwitch.checked = true;
         document.getElementById('switch-icon').setAttribute('class', 'fa-solid fa-moon');
+        document.getElementById('switch-image').setAttribute('src', './public/images/footer/Logo_Light.png');
     }
 }
 
